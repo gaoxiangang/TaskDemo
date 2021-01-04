69b6a59fab6b9c388be10a68a4df1abe0319ed9b
444 <1209247128@qq.com>
2021-01-04 23:21:39 +0800

    添加注释

--- Reactive ExtensionsDemo/Reactive ExtensionsDemo/Rx-Recipe2.cs
+++ Reactive ExtensionsDemo/Reactive ExtensionsDemo/Rx-Recipe2.cs
@@ -10,6 +10,10 @@ namespace Reactive_ExtensionsDemo
 {
     public class Rx_Recipe2
     {
+        /// <summary>
+        /// 声明一个可枚举集合
+        /// </summary>
+        /// <returns></returns>
         public static IEnumerable<int> EnumerableEventSequence()
         {
             for (int i = 0; i < 10; i++)
@@ -24,6 +28,7 @@ namespace Reactive_ExtensionsDemo
         {
             var st = new Stopwatch();
             st.Start();
+            //从集合中循环打印数据
             foreach (int i in EnumerableEventSequence())
             {
                 WriteLine(i);
@@ -34,16 +39,21 @@ namespace Reactive_ExtensionsDemo
             WriteLine($"花费{st.Elapsed}");
 
             st.Restart();
+            //将集合转换成可观察的集合
             IObservable<int> observable = EnumerableEventSequence().ToObservable();
 
+            //Subscribe 订阅该集合的更新
             using (IDisposable subscription = observable.Subscribe(Write))
             {
+                //在每次更新该集合时打印数据 类似做切换数据库服务器操作
                 WriteLine();
                 WriteLine("IObservable");
                 WriteLine($"花费{st.Elapsed}");
             }
 
             st.Restart();
+
+            //SubscribeOn将集合转换成可观察的异步集合，并放入TPL任务池中，并卸载主线程的任务
             observable = EnumerableEventSequence().ToObservable()
                 .SubscribeOn(TaskPoolScheduler.Default);
             using (IDisposable subscription =observable.Subscribe(Write))
--- Reactive ExtensionsDemo/Reactive ExtensionsDemo/Rx-Recipe3.cs
+++ Reactive ExtensionsDemo/Reactive ExtensionsDemo/Rx-Recipe3.cs
@@ -10,6 +10,14 @@ namespace Reactive_ExtensionsDemo
 {
     public class Rx_Recipe3
     {
+        /*********
+         * CustomObserver 自定义观察操作方法 /CustomSquence自定义集合
+         * 声明CustomObserver 然后声明两个不同的CustomSquence集合，一个可以正常运行 一个则会报错
+         * 我们在CustomSquence中并未检测数据是否正确以此来引发错误    PS记得Crtl+F5运行程序
+         * 第一个则是正常的观察者模式。未执行任何监测更新操作
+         * 第二个则是异步的观察者集合 执行中会和第一个操作打印相同的数据
+         * 第三个也是异步的观察者集合，不同的是该集合会因为 传入的null而导致执行错误
+         * **/
 
         public static void RunMain()
         {
